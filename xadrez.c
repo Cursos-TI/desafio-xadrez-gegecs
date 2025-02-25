ddc61385f4c16db6db35c48e0739a32f6229fff8
Geovanni <145285671+gegecs@users.noreply.github.com>
2025-02-25 21:38:14 +0000

    Movimentação do Cavalo adicionada

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,9 @@
 int main() {
     // Declaração da variável para a repetição.
     int i = 1;
+    // Variáveis para a movimentação do cavalo.
+    int cavaloMov, j = 1;
+
     printf("Bispo:\n");
     // while para imprimir a movimentação do bispo 5 vezes.
     while (i <= 5) {
@@ -29,6 +32,19 @@ int main() {
         i++;
     } while (i <= 8);
 
+    //Divisão das peças para o cavalo
+    printf("---------------------------------------\n");
+    printf("Cavalo:\n");
+
+    for(cavaloMov = 1; cavaloMov == 1; cavaloMov++) {
+        // While para imprimir "Baixo" 2 vezes
+        while(j <= 2) {
+            printf("Baixo - ");
+            j++;
+        }
+        // printf para imprimir "Esquerda" após a finalização do loop while
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
